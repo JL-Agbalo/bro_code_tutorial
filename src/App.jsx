738f1b842789d3156f3400e125a5dfd14a31942d
fishagbalo <fishagbalo@gmail.com>
2024-06-02 23:16:50 +0800

    Using Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 // import Card from "./Card";
-import Button from "./Button";
+import Student from "./Student";
+// import Button from "./Button";
+import UserGreeting from "./UserGreeting"
 function App() {
 return( 
   <>
   {/* <Card/> */}
-  <Button></Button>
+  {/* <Student name="Lenard" age={24}  isStudent={true}></Student>
+  <Student name="Strong" age={23}  isStudent={false}></Student>
+  <Student name="Fish" age={27}  isStudent={true}></Student>
+  <Student name="Fishdol" age={21}  isStudent={true}></Student>
+  <Student name="Larry" ></Student> */}
+  {/* <Button></Button> */}
+  <UserGreeting isLoggedIn={true} username="LenardCode"></UserGreeting>
+  <UserGreeting isLoggedIn={true} ></UserGreeting>
+  <UserGreeting ></UserGreeting>
   </>
   );
 }
