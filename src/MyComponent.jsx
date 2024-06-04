e2254e4c525d5031fdb076559b41c7fb365f9c24
fishagbalo <fishagbalo@gmail.com>
2024-06-04 23:43:14 +0800

    adding aage

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,16 +1,23 @@
 import React, {useState} from 'react'
 
 function MyComponent() {
-
-    const [name, setName] = useState("")
+    const [age, setAge] = useState(0)
+    const [name, setName] = useState("Guest")
     const updateName = () => {
         setName("Lenard")
+        // console.log(name)
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1)
         // console.log(name)
     }
   return (
     <div>
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
     </div>
   )
 }
