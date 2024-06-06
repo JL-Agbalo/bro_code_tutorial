0547f98b7595e76eead73a606a492f7f34fa2bca
fishagbalo <fishagbalo@gmail.com>
2024-06-06 23:17:05 +0800

    Select Option

--- src/MyComponent2.jsx
+++ src/MyComponent2.jsx
@@ -4,6 +4,8 @@ function MyComponent2() {
     const [name, setName] = useState("")
     const [quantity, setQuantity] = useState(1)
     const [comment, setComment] = useState("")
+    const [payment, setPayment] = useState()
+
     function handleNameChange(event){
         setName(event.target.value)
     }
@@ -15,6 +17,10 @@ function MyComponent2() {
     function handleCommentChange(event){
         setComment(event.target.value)
     }
+
+    function handlepaymentChange(event){
+        setPayment(event.target.value)
+    }
     return (
     <div>
         <input type="text" value={name} onChange={handleNameChange} />
@@ -23,6 +29,14 @@ function MyComponent2() {
         <p>Quantity: {quantity}</p>
         <textarea name="" value={comment} id="" placeholder='Enter delivery instructions' onChange={handleCommentChange}></textarea>
         <p>Comment: {comment}</p>
+
+        <select name="" id="" value={payment} onChange={handlepaymentChange}>
+            <option value="">Select and Option</option>
+            <option value="Visa">Visa</option>
+            <option value="Mastercard">Master Card</option>
+            <option value="Giftcard">Gift Card</option>
+        </select>
+        <p>Payment: {payment}</p>
     </div>
   )
 }
