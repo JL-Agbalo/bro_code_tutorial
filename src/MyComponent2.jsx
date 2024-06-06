5650b69bd112cd09cb0beefafec3d53ad8691d4d
fishagbalo <fishagbalo@gmail.com>
2024-06-06 23:12:45 +0800

    Adding text area and quantity change;

--- src/MyComponent2.jsx
+++ src/MyComponent2.jsx
@@ -2,13 +2,27 @@ import React, { useState } from 'react'
 
 function MyComponent2() {
     const [name, setName] = useState("")
+    const [quantity, setQuantity] = useState(1)
+    const [comment, setComment] = useState("")
     function handleNameChange(event){
         setName(event.target.value)
+    }
+
+    function handleQuantityChange(event){
+        setQuantity(event.target.value)
+    }
+
+    function handleCommentChange(event){
+        setComment(event.target.value)
     }
     return (
     <div>
         <input type="text" value={name} onChange={handleNameChange} />
         <p>Name: {name}</p>
+        <input type="number" value={quantity} onChange={handleQuantityChange} />
+        <p>Quantity: {quantity}</p>
+        <textarea name="" value={comment} id="" placeholder='Enter delivery instructions' onChange={handleCommentChange}></textarea>
+        <p>Comment: {comment}</p>
     </div>
   )
 }
