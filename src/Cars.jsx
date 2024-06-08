414592c0e8e25e5b9c6167f04174f65496c73a81
fishagbalo <fishagbalo@gmail.com>
2024-06-08 23:31:27 +0800

    Adding Remove state

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -8,18 +8,47 @@ function Cars() {
     const [carModel, setCarModel] = useState("");
 
     function handleAddCar(){
+        const newCar = {
+            year: carYear, 
+            make: carMake, 
+            model: carModel}
 
+            setCars(c => [...c, newCar])
+            setCarYear(new Date().getFullYear());
+            setCarMake("")
+            setCarModel("")
     }
 
     function handleRemoveCar(index){
-
+        setCars(c => c.pushfilter((_, i) => i !== index)); // if naka _ is hindi ginagamit, _ to ignote this parameter
     }
 
     function handleYearChange(){
+        setCarYear(event.target.value);
+    }
+
+    function handleMakeChange(){
+        setCarMake(event.target.value);
+
+    }
+    function handleModelChange(){
+        setCarModel(event.target.value);
         
     }
   return (
-    <div>Cars</div>
+    <div>
+        <h2>List of Car Object</h2>
+        <ul>
+            {cars.map((car, index) =>
+            <li key={index} onClick={() => handleRemoveCar(index)}>
+                {car.year} {car.make} {car.model}
+            </li>)}
+        </ul>
+        <input type="number" value ={carYear} onChange={handleYearChange}/><br></br>
+        <input type="text" value ={carMake} onChange={handleMakeChange} placeholder='Enter car make'/><br></br>
+        <input type="text" value ={carModel} onChange={handleModelChange} placeholder='Enter car model'/><br></br>
+        <button onClick={handleAddCar}>Add Car</button>
+    </div>
   )
 }
 
