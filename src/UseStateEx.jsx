9301158dffd1f2f1b1c2e73027fdf3f329f4f294
fishagbalo <fishagbalo@gmail.com>
2024-06-10 23:30:52 +0800

    chagning by hooks

--- src/UseStateEx.jsx
+++ src/UseStateEx.jsx
@@ -4,21 +4,31 @@ function UseStateEx() {
 
     const [count, setCount] = useState(0);
 
-
+    const[color, setColor] = useState ("green");
     // useEffect(() => {document.title=`Count: ${count}`});  // Runs after every re-render
-    // function addCount(){
-    //     setCount(c => c + 1);
-    // }
 
-    useEffect(() => {document.title=`Count: ${count}`}, []);
+    // useEffect(() => {document.title=`Count: ${count}`}, []); // Run only on mount
+    useEffect(() => {document.title=`Count: ${count} ${color}`}, [count, color]);
 
     function addCount(){
         setCount(c => c + 1);
     }
+
+    function subtractCount(){
+        setCount(c => c - 1);
+    }
+
+    function changeColor(){
+        setColor(c => c === "green"? "red" : "green") 
+    }
+
   return (
     <div>
-        <p>Count: {count}</p>
+        <p style={{color: color}}>Count: {count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button>
+        <br />
+        <button onClick={changeColor}>Change Color</button>
 
     </div>
   )
