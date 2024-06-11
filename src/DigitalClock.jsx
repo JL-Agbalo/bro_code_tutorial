ec76899bb914b50362236c992efd8f9d14cf9f09
fishagbalo <fishagbalo@gmail.com>
2024-06-11 23:34:40 +0800

    adding css for clock

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react'
 
 export default function DigitalClock() {
   return (
-    <div>DigitalClock</div>
+    <div className='clock-container'>
+        <div className='clock'>
+          <span>00:00:00</span>
+        </div>
+    </div>
   )
 }
