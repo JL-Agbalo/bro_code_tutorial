a7d2edd1c2785eab55ae53c27cec8c1e8fb282a5
fishagbalo <fishagbalo@gmail.com>
2024-06-15 23:43:18 +0800

    component render strict mode

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 function UseRef() {
+    let [number, setNumber] = useState(0)
+    useEffect(() => {
+        console.log("Component Rendered")
+    })
+    function handleClick(){
+        setNumber(n => n + 1)
+    }
+    
   return (
-    <div>UseRef</div>
+    <div>
+        <button onClick={handleClick}>Click Me!</button>
+    </div>
   )
 }
 
