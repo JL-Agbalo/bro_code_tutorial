3b11caf1359056459911a540a9dc1709c95246bc
fishagbalo <fishagbalo@gmail.com>
2024-06-15 23:51:51 +0800

    Component render using inputref

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -1,17 +1,23 @@
 import React, {useState, useRef, useEffect} from 'react'
 
 function UseRef() {
-    let [number, setNumber] = useState(0)
+    // let [number, setNumber] = useState(0)
+
+    const inputRef = useRef(null);
+
     useEffect(() => {
         console.log("Component Rendered")
+        console.log("Input Ref: ", {inputRef})
     })
     function handleClick(){
-        setNumber(n => n + 1)
+        // setNumber(n => n + 1)
+        inputRef.current.focus();
     }
     
   return (
     <div>
         <button onClick={handleClick}>Click Me!</button>
+        <input type="text" ref={inputRef} />
     </div>
   )
 }
