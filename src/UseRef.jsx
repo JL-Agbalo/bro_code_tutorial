00a01407427c07d19fda4c97038485ff43c24a4f
fishagbalo <fishagbalo@gmail.com>
2024-06-15 23:56:27 +0800

    adding inputs to test the useRef

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -3,21 +3,52 @@ import React, {useState, useRef, useEffect} from 'react'
 function UseRef() {
     // let [number, setNumber] = useState(0)
 
-    const inputRef = useRef(null);
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
 
     useEffect(() => {
         console.log("Component Rendered")
-        console.log("Input Ref: ", {inputRef})
+        // console.log("Input Ref: ", {inputRef})
     })
-    function handleClick(){
+    function handleClick1(){
         // setNumber(n => n + 1)
-        inputRef.current.focus();
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor ="yellow"
+        inputRef2.current.style.backgroundColor =""
+        inputRef3.current.style.backgroundColor =""
+  
     }
     
+    function handleClick2(){
+        // setNumber(n => n + 1)
+        inputRef2.current.focus();
+        inputRef2.current.style.backgroundColor ="yellow"
+        inputRef1.current.style.backgroundColor =""
+        inputRef3.current.style.backgroundColor =""
+    
+    }
+
+    function handleClick3(){
+        // setNumber(n => n + 1)
+        inputRef3.current.focus();
+        inputRef3.current.style.backgroundColor ="yellow"
+        inputRef1.current.style.backgroundColor =""
+        inputRef2.current.style.backgroundColor =""
+    }
   return (
     <div>
-        <button onClick={handleClick}>Click Me!</button>
-        <input type="text" ref={inputRef} />
+        <button onClick={handleClick1}>Click Me 1!</button>
+        <input type="text" ref={inputRef1} />
+
+        <br />
+        <button onClick={handleClick2}>Click Me 2!</button>
+        <input type="text" ref={inputRef2} />
+
+        <br />
+
+        <button onClick={handleClick3}>Click Me 3!</button>
+        <input type="text" ref={inputRef3} />
     </div>
   )
 }
